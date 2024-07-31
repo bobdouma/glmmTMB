9be99b02ac596a10a6c41dfe6d81b841e3939ee6
Bob Douma <bob.douma@wur.nl>
2024-07-31 13:44:41 +0200

    commenting out

--- glmmTMB/src/glmmTMB.cpp
+++ glmmTMB/src/glmmTMB.cpp
@@ -332,20 +332,20 @@ Type termwise_nll(array<Type> &U, vector<Type> theta, per_term_info<Type>& term,
          }
     }
 
-    // // Replace 998 and compute new values
+    // Replace 998 and compute new values
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++) {
             if (theta_cov_mat(i, j) > 998) {
                 if (j == 0) {
                     theta_cov_mat(i, j) = 0;  // First column exception
                 } else if (j > 0) {
-                    double Ljj = theta_cov_mat(j, j);
-                    double sum_term = 0.0;
-						for (int k = 0; k < j; k++) {
-							sum_term += theta_cov_mat(i, k) * theta_cov_mat(j, k);
+    //                double Ljj = theta_cov_mat(j, j);
+    //                double sum_term = 0.0;
+	//					for (int k = 0; k < j; k++) {
+	//						sum_term += theta_cov_mat(i, k) * theta_cov_mat(j, k);
 						}
-                    double Lij = (0 - sum_term) / Ljj;
-                    theta_cov_mat(i, j) = Lij;
+    //                double Lij = (0 - sum_term) / Ljj;
+    //                theta_cov_mat(i, j) = Lij;
                 }
             }
         }
